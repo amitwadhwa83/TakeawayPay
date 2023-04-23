6c5abcac6e603ac3384d2bd47764a0e08eef5351
amit wadhwa <amitwadhwa83@gmail.com>
2023-04-23 20:23:22 +0200

    remove documentation

--- src/main/java/com/takeaway/pay/TakeawayPayApplication.java
+++ src/main/java/com/takeaway/pay/TakeawayPayApplication.java
@@ -15,15 +15,4 @@ public class TakeawayPayApplication {
         SpringApplication.run(TakeawayPayApplication.class, args);
     }
 
-    @Bean
-    public OpenAPI mortageOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Takeaway Pay application")
-                        .description("Backend test for Just Eat Takeaway.com Operations Finance.")
-                        .version("v0.0.1")
-                        .contact(new Contact()
-                                .name("mortgage_help@ing.com"))
-                        .license(new License().name("ING 1.0").url("https://www.ing.com.tr/en/knowledge-base/mortgage")));
-    }
 }
